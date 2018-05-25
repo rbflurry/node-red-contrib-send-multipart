d4b590cef23ff209bc7f77a04b48c232e7b0622e
Elizabeth Southerland <Elizabeth.Southerland@sbdinc.com>
2018-05-25 10:02:35 -0400

    readme file fixed?

--- README.md
+++ README.md
@@ -1,17 +1,25 @@
 # node-red-contrib-send-multipart
-This is a node-red node for posting http(s) requests with multipart formData. Currently a work in progress
-Right now this node is only looking for files. However, there are future plans to handle all multipart/form-data.
+This is a node-red node for posting http(s) requests containing files as multipart formData. Currently a work in progress.
+As of now this node is only looking for files. However, there are future plans to handle all multipart/form-data.
 
 ## Installation
 run npm -g install node-red-contrib-send-multipart
 
-<!-- [![npm package](https://nodei.co/npm/node-red-contrib-http-request.png?downloads=true&downloadRank=true&stars=true)](https://nodei.co/npm/node-red-contrib-http-request/) -->
-
 ## Features
 Pulled together using some of the best of parts of other node-red-contrib nodes (particularly node-red-contrib-http-request), and the best parts of stackoverflow.
-Currently only accepts & sends files. However, there are future plans to handle all multipart/form-data. 
+Currently only sends files. However, there are future plans to handle other types multipart/form-data.
 
-## Why this module ?
-As of May 2018, NodeRed does not yet support sending multipart form-data.  This module serves to fill that gap.
+## Usage
+Required inputs: url (this is specified on the node) & filepath (path to the file to be sent)
+Filepath can be indicated 2 ways:
+(1) Explicitly state the filepath on the node (useful if filepath is a constant)
+(2) Pass the filepath into the node as part of the msg, as "msg.filepath"
+
+
+## Why this module?
+As of May 2018, NodeRed does not yet support sending multipart form-data.  This module aims to begin to close that gap.
 
 There is always room for improvement, and new ideas are valued.  Feel free to submit pull requests to make this library even better.
+
+## What's next?
+In the future, I definitely want to add more support for other data types, as well as more flexibility in the model.  For example, I'd for users to be able to just pass in a file instead of read it from local.  I developed this node for a very specific use case, which is why I haven't dived into all that yet.  It's on my to-do list!
--- http-send-multipart.js
+++ http-send-multipart.js
@@ -1,11 +1,11 @@
 // require in libs
-var	mustache = require('mustache'),
+var mustache = require('mustache'),
 	request = require('request'),
 	http = require('http'),
 	https = require('https'),
 	fs = require('fs');
 
-var filepath = "sanityCheck.csv"; // initializing filepath
+var filepath = "default.csv"; // initializing filepath
 
 module.exports = function(RED) {
 
@@ -27,82 +27,96 @@ module.exports = function(RED) {
 		// 1) Process inputs to Node
 		this.on("input", function(msg) {
 
-			// Look for filepath - // TODO improve / document
-			if (n.filepath) {
+			// TODO: add ability to select other input types (not just files)
+
+			// Look for filepath - // TODO improve logic
+
+			if (!n.filepath && !msg.filepath) {
+				// throw an error if no filepath
+				node.warn(RED._("Error: no filepath found to send file."));
+				msg.error = "Filepath was not defined";
+				msg.statusCode = 400;
+				node.send(msg); // TODO: make sure this escapes entirely; need better error-handling here
+			} else {
+				if (n.filepath) {
 					filepath = n.filepath;
-			} else if (msg.filepath) {
+				} else if (msg.filepath) { // TODO: improve logic
 					filepath = msg.filepath;
-			}
+				}
 
-			var preRequestTimestamp = process.hrtime();
-			node.status({
-				fill: "blue",
-				shape: "dot",
-				text: "Sending multipart request..."
-			});
-			var url = nodeUrl; // TODO add ability to take this from the settings.js config file
-			if (isTemplatedUrl) {
-				url = mustache.render(nodeUrl, msg);
-			}
-			if (!url) {
-				node.error(RED._("httpSendMultipart.errors.no-url"), msg);
+				var preRequestTimestamp = process.hrtime();
 				node.status({
-					fill: "red",
-					shape: "ring",
-					text: (RED._("httpSendMultipart.errors.no-url"))
+					fill: "blue",
+					shape: "dot",
+					text: "Sending multipart request..."
 				});
-				return;
-			}
-
-
-			// Add auth if it exists
-			if (this.credentials && this.credentials.user) {
-				var urlTail = url.substring(url.indexOf('://') + 3); // hacky but it works. don't judge me
-				var username = this.credentials.user,
-					password = this.credentials.password;
-				url = 'https://' + username + ':' + password + '@' + urlTail;
-
-			}
-
-			var respBody, respStatus;
-
-			var thisReq = request.post(url, function(err, resp, body) {
-
-				node.status({});
-				if (err || !resp) {
-					// node.error(RED._("httpSendMultipart.errors.no-url"), msg);
-					var statusText = "Unexpected error";
-					if (err) {statusText = err;} else if (!resp) { statusText = "No response object"; }
+				var url = nodeUrl; // TODO add ability to take this from the settings.js config file
+				if (isTemplatedUrl) {
+					url = mustache.render(nodeUrl, msg);
+				}
+				if (!url) {
+					node.error(RED._("httpSendMultipart.errors.no-url"), msg);
 					node.status({
 						fill: "red",
 						shape: "ring",
-						text:statusText
+						text: (RED._("httpSendMultipart.errors.no-url"))
 					});
+					return;
 				}
-				msg.payload = body;
-				msg.statusCode = resp.statusCode || resp.status;
-				msg.headers = resp.headers;
 
-				if (node.ret !== "bin") {
-					msg.payload = body.toString('utf8'); // txt
 
-					if (node.ret === "obj") {
-						try {
-							msg.payload = JSON.parse(body);
+				// Add auth if it exists
+				if (this.credentials && this.credentials.user) {
+					var urlTail = url.substring(url.indexOf('://') + 3); // hacky but it works. don't judge me
+					var username = this.credentials.user,
+						password = this.credentials.password;
+					url = 'https://' + username + ':' + password + '@' + urlTail;
+
+				}
+
+				var respBody, respStatus;
+
+				var thisReq = request.post(url, function(err, resp, body) {
+
+					node.status({});
+					if (err || !resp) {
+						// node.error(RED._("httpSendMultipart.errors.no-url"), msg);
+						var statusText = "Unexpected error";
+						if (err) {
+							statusText = err;
+						} else if (!resp) {
+							statusText = "No response object";
 						}
-						catch (e) {
-							node.warn(RED._("httpSendMultipart.errors.json-error"));
+						node.status({
+							fill: "red",
+							shape: "ring",
+							text: statusText
+						});
+					}
+					msg.payload = body;
+					msg.statusCode = resp.statusCode || resp.status;
+					msg.headers = resp.headers;
+
+					if (node.ret !== "bin") {
+						msg.payload = body.toString('utf8'); // txt
+
+						if (node.ret === "obj") {
+							try {
+								msg.payload = JSON.parse(body);
+							} catch (e) {
+								node.warn(RED._("httpSendMultipart.errors.json-error"));
+							}
 						}
 					}
-				}
 
-				node.send(msg);
-			});
-			var form = thisReq.form();
-			form.append('file', fs.createReadStream(filepath), {
-				filename: filepath,
-				contentType: 'multipart/form-data'
-			});
+					node.send(msg);
+				});
+				var form = thisReq.form();
+				form.append('file', fs.createReadStream(filepath), {
+					filename: filepath,
+					contentType: 'multipart/form-data'
+				});
+			}
 
 		}); // end of on.input
 
